0ccba4027947021ffc3749bc5788a32fbd932c44
Veit Gossen <veitgossen@arcor.de>
2011-11-01 13:44:23 +0100

    new files

---
+++ content/herzogtum_nebula/herzogtum_nebula.php
@@ -0,0 +1,14 @@
+<?php include '../../includes/header.html'; ?>
+
+<?php include "../../empty_navigation.html"; ?>	
+
+<?php $title = 'Herzogtum Nebula'; ?>
+
+<?php include 'path_self.php'; ?>			
+			
+
+<div>
+	<p>Diese Seite befindet sich noch im Aufbau</p>	
+</div>
+
+<?php include '../../includes/footer.html'; ?>
---
+++ content/raiglaender_soeldner/ueber_uns.php
@@ -0,0 +1,30 @@
+<?php include '../../includes/header.html'; ?>
+
+<?php include "navigation.html"; ?>	
+
+<?php $title = 'Über uns'; ?>
+
+<?php include 'path.php'; ?>	
+
+<div class="vbox">
+	<p><a href="referenzen.php" target="_blank" onclick="return openWindow('referenzen.php','Hauptmann Darek');"><img src="/images/no_img.jpg" width="113" height="100"/></a></p>
+	<h1><a href="referenzen.php" target="_blank" onclick="return openWindow('referenzen.php','Hauptmann Darek');">Hauptmann Darek</a></h1>
+</div>
+
+<div class="vbox">
+	<p><a href="charaktere/adrian.html" target="_blank" onclick="return openWindow('charaktere/adrian.html','Adrian van Trebe');"><img src="/images/no_img.jpg" width="113" height="100"/></a></p>
+	<h1><a href="charaktere/adrian.html" target="_blank" onclick="return openWindow('charaktere/adrian.html','Adrian van Trebe');">Adrian van Trebe</a></h1>
+	<h1></h1>
+</div>
+
+<div class="vbox">
+	<img src="/images/no_img.jpg" width="113" height="100"/>
+	<h1>Wilhelm van Bram</h1>
+</div>
+
+<div class="vbox">
+<img src="/images/no_img.jpg" width="113" height="100"/>
+<h1>Conrad von Freifels</h1>
+</div>
+
+<?php include '../../includes/footer.html'; ?>
