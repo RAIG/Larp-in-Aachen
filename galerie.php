fad9de3e87b5c21fc993d5f80d8053157ffd920c
Veit Gossen <veitgossen@arcor.de>
2012-02-11 13:47:58 +0100

    new lightweight footer design

--- galerie.php
+++ galerie.php
@@ -2,37 +2,7 @@
 
 <?php include 'includes/header.php'; ?>
 
-<div class="row">
-	<div class="span6 offset3">
-		<div id="myCarousel" class="carousel slide">
-			<div class="carousel-inner">
-				<div class="item active">
-					<img src="img/conquest2008.jpg" alt="">
-					<div class="carousel-caption">
-						<h4>First Thumbnail label</h4>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-					</div>
-				</div>
-				<div class="item">
-					<img src="img/conquest2009.jpg" alt="">
-					<div class="carousel-caption">
-						<h4>Second Thumbnail label</h4>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-					</div>
-				</div>
-				<div class="item">
-					<img src="img/hamarsmark2008.jpg" alt="">
-					<div class="carousel-caption">
-						<h4>Third Thumbnail label</h4>
-						<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-					</div>
-				</div>
-			</div>
-			<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-			<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-		</div>
-	</div>
-</div>
+
 
 <div class="row">
 	<div class="span4">
--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,11 @@
 
-</div> 
-
-<footer class="footer">  
-	<div class="container">
-		<div class="row">
-			<div class="span12">
-
-			<p class="pull-left"><a href="">Impressum</a></p>
-			<p class="pull-left"><a href="">Sitemap</a></p>
-			<p class="pull-right"><a href="#">Nach oben<i class="icon-arrow-up icon-white"></i></a></p>
-			</div>			
-		</div>
-		<div class="footer-underline">
-			<p class="pull-right">Inspiriert und entwickelt aus <a target="_blank" href="http://twitter.github.com/bootstrap/">Bootstrap</a>, ein Front-end Toolkit von <a target="_blank" href="http://twitter.com/twitter">Twitter</a></p>		
-		</div>		
-	</div>
-</footer>
-
+	<footer class="footer"> 
+		<p class="pull-left"><a href="">Impressum</a></p>
+		<p class="pull-left"><a href="">Sitemap</a></p>
+		<p class="pull-right"><a href="#">Nach oben<i class="icon-arrow-up"></i></a></p>
+		
+	</footer>
+</div>
 <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="js/bootstrap-modal.js"></script>
